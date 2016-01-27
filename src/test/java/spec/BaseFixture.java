f4ab531a9ddf798bfdde9ba462171905d6bab1c1
Andrew Sumner <andrew.sumner@assurity.co.nz>
2016-01-27 14:39:04 +1300

    Get failing

--- src/test/java/spec/BaseFixture.java
+++ src/test/java/spec/BaseFixture.java
@@ -1,11 +1,10 @@
 package spec;
 
 import org.concordion.api.AfterSpecification;
+import org.concordion.api.AfterSuite;
 import org.concordion.api.BeforeSpecification;
+import org.concordion.api.BeforeSuite;
 import org.concordion.api.FailFast;
-import org.concordion.api.extension.Extension;
-import org.concordion.ext.LoggingFormatterExtension;
-import org.concordion.ext.loggingFormatter.LogbackHelper;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -18,29 +17,23 @@ import org.slf4j.LoggerFactory;
 public abstract class BaseFixture {
 	
     private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
-    
-//    private static TestData testData = new TestData();
-    
-    @Extension public final LoggingFormatterExtension loggingFormatter = new LoggingFormatterExtension();
-//    @Extension public final SpecificationProcessingExtension specificationProcessing = new SpecificationProcessingExtension(this);
 	
-//    public TestData testData() {
-//    	return testData;
-//    }
-	
-    static {
-    	LogbackHelper.logInternalStatus();
-	}
-    
-	public Logger getLogger() {
+    public Logger getLogger() {
 		return logger;
 	}
 	
+	@BeforeSuite
+	final public void beforeSuite() {
+		System.out.println("@BeforeSuite");
+	}
+	
+	@AfterSuite
+	final public void afterSuite() {
+		System.out.println("@AfterSuite");
+	}
 	
     @BeforeSpecification
-    final public void startUp() {
-    	LogbackHelper.startTestLogging(this);
-    	
+    final public void beforeSpecification() {
     	// This is the name that can be given to the RunSingleTest job in Jenkins
     	String testName = this.getClass().getName().replace(BaseFixture.class.getPackage().getName() + ".", "");
     	
@@ -48,10 +41,8 @@ public abstract class BaseFixture {
     }
 
     @AfterSpecification
-    final public void tearDown() throws Exception {
+    final public void afterSpecification() throws Exception {
     	logger.info("Tearing down the acceptance test class {} on thread {}", this.getClass().getSimpleName(), Thread.currentThread().getName());
-    	
-    	LogbackHelper.stopTestLogging();
 	}
 }
 
--- src/test/java/spec/RunSingleTest.java
+++ src/test/java/spec/RunSingleTest.java
@@ -5,7 +5,8 @@ import org.concordion.api.BeforeSpecification;
 import org.junit.Rule;
 import org.junit.rules.TestWatcher;
 
-public class RunSingleTest extends BaseFixture {
+
+public class RunSingleTest extends BaseFixture {	
 	String testName;
 	
     @Rule
@@ -15,18 +16,18 @@ public class RunSingleTest extends BaseFixture {
     		testName = description.getMethodName();	
     	};
 	};
-    
+	
 	@BeforeSpecification
 	public void startTest() {
 		getLogger().error("START " + this.getClass().getSimpleName());
-//		testData().value = "b";
+		throw new RuntimeException("Lets bail outta here!");
 	}
 	
 	@AfterSpecification
 	public void endTest() {
 		getLogger().error("END " + this.getClass().getSimpleName());
 	}
-	
+    
     public boolean log(String message) {
     	getLogger().warn(testName + " logging " + message);
     	
